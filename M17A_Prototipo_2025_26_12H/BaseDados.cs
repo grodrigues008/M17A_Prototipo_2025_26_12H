7494ac131b1cfb8960d0dd5c495f1c19a34357bb
Gonçalo Rodrigues <a24586@esenviseu.net>
2025-11-12 10:14:40 +0000

    final da aula

---
+++ M17A_Prototipo_2025_26_12H/BaseDados.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.Data.SqlClient;
+
+namespace M17A_Prototipo_2025_26_12H
+{
+    /// <summary>
+    /// Responsável por executar comandos SQL na base de dados
+    /// </summary>
+
+    internal class BaseDados
+    {
+        string strligacao;
+        string NomeBD;
+        string CaminhoBD;
+        SqlConnection ligacaoSQL;
+
+
+        //construtor
+        // Estabelece a ligação à BD, caso não exista cria a BD
+        public BaseDados(string NomeBD)
+        {
+            this.NomeBD = NomeBD;
+            //Ler a string ligação
+            strligacao = ConfigurationManager.ConnectionStrings["sql"].ToString();
+            // Verificar a pasta do projeto
+            CaminhoBD = Utils.PastaDoPrograma("M17A_Biblioteca");
+            CaminhoBD += @"\" + NomeBD + ".mdf";
+            // Verificar se a bd existe
+            if (System.IO.File.Exists(CaminhoBD) == false)
+            {
+                // Se não existir
+                // criar a bd
+                //TODO: verificar se a bd existe no catálogo
+                CriarBD();
+            }
+
+            // Ligação à BD
+        }
+
+
+        // desconstrutor
+        ~BaseDados()
+        {
+            //fechar a ligação à bd
+        }
+
+        void CriarBD()
+        {
+            //ligação ao servidor
+            ligacaoSQL = new SqlConnection(strligacao);
+            ligacaoSQL.Open();
+            //criar a bd
+            SqlCommand comando = new SqlCommand();
+            // criar as tabelas
+        }
+
+    }
+}
---
+++ M17A_Prototipo_2025_26_12H/Utils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace M17A_Prototipo_2025_26_12H
+{
+    /// <summary>
+    /// Funções static para ler imagens, criar ficheiros de imagens
+    /// e saber a pasta do programa 
+    /// </summary>
+
+    internal class Utils
+    {
+        /// <summary>
+        /// Verifica se a pasta existe e se necessário cria a pasta
+        /// </summary>
+        /// <param name="nome">Nome da Pasta</param>
+        /// <returns>Devolve o caminho completo</returns>
+
+        public static string PastaDoPrograma(string nome)
+        {
+            // Caminho completo para a pasta appdata\local
+            string pastainicial = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            pastainicial += @"\" + nome;
+            if (System.IO.Directory.Exists(pastainicial) == false)
+                System.IO.Directory.CreateDirectory(pastainicial);
+            return pastainicial;
+        }
+        //TODO: Funções para imagens
+    }
+}
